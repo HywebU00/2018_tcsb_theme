125b8790fe997b54d8766f35ecd19e594c62d08c
anniewu <annie@hyweb.com.tw>
2023-05-11 14:32:26 +0800

    20230511修改各版頁籤標題html及css、js

--- RWD_01/js/hyui.js
+++ RWD_01/js/hyui.js
@@ -206,7 +206,6 @@ $(function() {
             $('.m_search').stop(true, false).slideDown('400', 'easeOutQuint');
             _window.off('resize');
             search_mode = true;
-            
         } else {
             $('.m_search').hide();
             search_mode = false;
@@ -422,72 +421,70 @@ $(function() {
             }
         });
     }
-   /*-----------------------------------*/
-  ////////////////多組Tab////////////////
-  /*-----------------------------------*/
-  var tab_headerHeight = Math.floor($('.header').outerHeight(true));
-  var resizeTimer1;
-  _window.resize(function () {
-    clearTimeout(resizeTimer1);
-    resizeTimer1 = setTimeout(function () {
-      ww = _window.outerWidth();
-      tabSet();
-    }, 50);
-  });
-
-  function tabSet() {
-    $('.tabs').each(function () {
-      var _tab = $(this),
-        _tabItem = _tab.find('.tabItem'),
-        _tabContent = _tab.find('.tabContent'),
-        tabwidth = _tab.width(),
-        tabItemHeight = _tabItem.outerHeight(),
-        tabContentHeight = _tab.find('.active').next().innerHeight(),
-        tabGutter = parseInt('4px'), // 可設定 Gutter 寬度
-        tabItemLength = _tabItem.length,
-        tabItemWidth,
-        marginLeft;
-      _tab.find('.active').next('.tabContent').show();
-      if (ww >= wwSmall) {
-        _tabContent.css('top', tabItemHeight);
-        _tab.height(tabContentHeight + tabItemHeight);
-
-        tabItemWidth = tabwidth / tabItemLength - tabGutter;
-        marginLeft = (tabwidth - tabItemWidth * tabItemLength) / (tabItemLength - 1);
-
-        _tabItem.outerWidth(tabItemWidth).css('margin-left', marginLeft);
-        _tabItem.first().css('margin-left', 0);
-        _tabItem.last().css({ position: 'absolute', top: 0, right: 0 }).outerWidth(tabItemWidth);
-      } else {
-        _tab.css('height', 'auto');
-        _tabItem.width(tabwidth);
-        _tabItem.css('margin-left', 0).last().css('position', 'relative');
-      }
-      _tabItem.focus(tabs); //改button後，前面改_tabItem
-      _tabItem.click(tabs); //改button後，前面改_tabItem
-      function tabs(e) {
-        var _tabItemNow = $(this), //改button後，原來$(this).parent(),改$(this)
-          tvp = _tab.offset().top,
-          tabIndex = _tabItemNow.index() / 2,
-          scollDistance = tvp + tabItemHeight * tabIndex - tab_headerHeight;
-        _tabItem.removeClass('active');
-        _tabItemNow.addClass('active');
-        if (ww <= wwSmall) {
-          _tabItem.not('.active').next().slideUp();
-          _tabItemNow.next().slideDown();
-          $('html,body').stop(true, false).animate({ scrollTop: scollDistance });
-        } else {
-          _tabItem.not('.active').next().hide();
-          _tabItemNow.next().show();
-          tabContentHeight = _tabItemNow.next().innerHeight();
-          _tab.height(tabContentHeight + tabItemHeight);
-        }
-        e.preventDefault();
-      }
+    /*-----------------------------------*/
+    ////////////////多組Tab////////////////
+    /*-----------------------------------*/
+    var tab_headerHeight = Math.floor($('.header').outerHeight(true));
+    var resizeTimer1;
+    _window.resize(function() {
+        clearTimeout(resizeTimer1);
+        resizeTimer1 = setTimeout(function() {
+            ww = _window.outerWidth();
+            tabSet();
+        }, 50);
     });
-  }
-  $('.tabs>.tabItem:first-child>a').trigger('click');
-  tabSet();
+
+    function tabSet() {
+        $('.tabs').each(function() {
+            var _tab = $(this),
+                _tabItem = _tab.find('.tabItem'),
+                _tabContent = _tab.find('.tabContent'),
+                tabwidth = _tab.width(),
+                tabItemHeight = _tabItem.outerHeight(),
+                tabContentHeight = _tab.find('.active').next().innerHeight(),
+                tabGutter = parseInt('4px'), // 可設定 Gutter 寬度
+                tabItemLength = _tabItem.length,
+                tabItemWidth,
+                marginLeft;
+            _tab.find('.active').next('.tabContent').show();
+            if (ww >= wwSmall) {
+                _tabContent.css('top', tabItemHeight);
+                _tab.height(tabContentHeight + tabItemHeight);
+                tabItemWidth = tabwidth / tabItemLength - tabGutter;
+                marginLeft = (tabwidth - tabItemWidth * tabItemLength) / (tabItemLength - 1);
+                _tabItem.outerWidth(tabItemWidth).css('margin-left', marginLeft);
+                _tabItem.first().css('margin-left', 0);
+                _tabItem.last().css({ position: 'absolute', top: 0, right: 0 }).outerWidth(tabItemWidth);
+            } else {
+                _tab.css('height', 'auto');
+                _tabItem.width(tabwidth);
+                _tabItem.css('margin-left', 0).last().css('position', 'relative');
+            }
+            _tabItem.focus(tabs); //改button後，前面改_tabItem
+            _tabItem.click(tabs); //改button後，前面改_tabItem
+            function tabs(e) {
+                var _tabItemNow = $(this), //改button後，原來$(this).parent(),改$(this)
+                    tvp = _tab.offset().top,
+                    tabIndex = _tabItemNow.index() / 2,
+                    scollDistance = tvp + tabItemHeight * tabIndex - tab_headerHeight;
+                _tabItem.removeClass('active');
+                _tabItemNow.addClass('active');
+                if (ww <= wwSmall) {
+                    _tabItem.not('.active').next().slideUp();
+                    _tabItemNow.next().slideDown();
+                    $('html,body').stop(true, false).animate({ scrollTop: scollDistance });
+                } else {
+                    _tabItem.not('.active').next().hide();
+                    _tabItemNow.next().show();
+                    tabContentHeight = _tabItemNow.next().innerHeight();
+                    _tab.height(tabContentHeight + tabItemHeight);
+                }
+                e.preventDefault();
+            }
+        });
+    }
+    $('.tabs>.tabItem:first-child>a').trigger('click');
+    tabSet();
     /*-----------------------------------*/
     ///////////////置頂go to top////////////
     /*-----------------------------------*/
--- RWD_05/js/hyui.js
+++ RWD_05/js/hyui.js
@@ -425,6 +425,7 @@ $(function() {
     /*-----------------------------------*/
     ////////////////多組Tab////////////////
     /*-----------------------------------*/
+    var tab_headerHeight = Math.floor($('.header').outerHeight(true));
     var resizeTimer1;
     _window.resize(function() {
         clearTimeout(resizeTimer1);
@@ -438,41 +439,41 @@ $(function() {
         $('.tabs').each(function() {
             var _tab = $(this),
                 _tabItem = _tab.find('.tabItem'),
-                _tabItemA = _tabItem.children('a'),
                 _tabContent = _tab.find('.tabContent'),
                 tabwidth = _tab.width(),
                 tabItemHeight = _tabItem.outerHeight(),
                 tabContentHeight = _tab.find('.active').next().innerHeight(),
-                tiGap = 5,
+                tabGutter = parseInt('4px'), // 可設定 Gutter 寬度
                 tabItemLength = _tabItem.length,
-                tabItemWidth;
+                tabItemWidth,
+                marginLeft;
             _tab.find('.active').next('.tabContent').show();
             if (ww >= wwSmall) {
                 _tabContent.css('top', tabItemHeight);
                 _tab.height(tabContentHeight + tabItemHeight);
-                tabItemWidth = (tabwidth - (tabItemLength - 1) * tiGap) / tabItemLength;
-                _tabItem.width(tabItemWidth).css('margin-left', tiGap);
+                tabItemWidth = tabwidth / tabItemLength - tabGutter;
+                marginLeft = (tabwidth - tabItemWidth * tabItemLength) / (tabItemLength - 1);
+                _tabItem.outerWidth(tabItemWidth).css('margin-left', marginLeft);
                 _tabItem.first().css('margin-left', 0);
-                _tabItem.last().css({ 'position': 'absolute', 'top': 0, 'right': 0 }).width(tabItemWidth);
+                _tabItem.last().css({ position: 'absolute', top: 0, right: 0 }).outerWidth(tabItemWidth);
             } else {
                 _tab.css('height', 'auto');
                 _tabItem.width(tabwidth);
                 _tabItem.css('margin-left', 0).last().css('position', 'relative');
             }
-            _tabItemA.focus(tabs);
-            _tabItemA.click(tabs);
-
+            _tabItem.focus(tabs); //改button後，前面改_tabItem
+            _tabItem.click(tabs); //改button後，前面改_tabItem
             function tabs(e) {
-                var _tabItemNow = $(this).parent(),
+                var _tabItemNow = $(this), //改button後，原來$(this).parent(),改$(this)
                     tvp = _tab.offset().top,
                     tabIndex = _tabItemNow.index() / 2,
-                    scollDistance = tvp + tabItemHeight * tabIndex - hh;
+                    scollDistance = tvp + tabItemHeight * tabIndex - tab_headerHeight;
                 _tabItem.removeClass('active');
                 _tabItemNow.addClass('active');
                 if (ww <= wwSmall) {
                     _tabItem.not('.active').next().slideUp();
                     _tabItemNow.next().slideDown();
-                    $("html,body").stop(true, false).animate({ scrollTop: scollDistance });
+                    $('html,body').stop(true, false).animate({ scrollTop: scollDistance });
                 } else {
                     _tabItem.not('.active').next().hide();
                     _tabItemNow.next().show();
